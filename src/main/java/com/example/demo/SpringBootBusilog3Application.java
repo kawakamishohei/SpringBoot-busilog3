e00c75b0df36b124779095e46e410e57512d491d
kawakamishohei <shohei.kawakami@enisias.com>
2023-05-26 16:04:49 +0900

    first commit

---
+++ src/main/java/com/example/demo/SpringBootBusilog3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBusilog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBusilog3Application.class, args);
+	}
+
+}
